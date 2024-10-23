858dd66bd9f7f1e42bdaf16daf8d2dfa7b492b1b
stefannguyen <stefan16.nguyen@gmail.com>
2024-10-23 09:37:55 +0200

    task8

--- Exercise 1/Aufgabe8.R
+++ Exercise 1/Aufgabe8.R
@@ -1,1 +1,21 @@
 # Aufgabe 8
+
+# Create a list 
+x_list <- list("John", "Mary", c(4, 6, 10))
+x_list
+
+# Create a second list containing the names Bob, Cate and Susan of the children
+y_list <- list("Bob", "Cate", "Susan")
+y_list
+
+# Concatenate the lists. 
+list_xy1 <- c(x_list, y_list)
+list_xy1
+
+list_xy2 <- list(x_list, y_list)
+list_xy2
+# Both work the same way but c() uses coercion, to convert all values to a 
+# specific type
+
+# Access the concatenated list to create directly a list containing for every
+# child name and age
