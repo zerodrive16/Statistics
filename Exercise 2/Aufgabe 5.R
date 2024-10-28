456b6538a4e348fe0fbcea1a02fbc9d1b0bb9a06
Stefan <110019272+zerodrive16@users.noreply.github.com>
2024-10-28 18:53:38 +0100

    exercise 2

--- Exercise 2/Aufgabe 5.R
+++ Exercise 2/Aufgabe 5.R
@@ -1,6 +1,6 @@
 # Aufgabe 5
 
-# (a) Load the libaries and nycflight13 and inspect the variable of flights
+# (a) Load the libraries and nycflight13 and inspect the variable of flights
 library(tidyverse)
 library(nycflights13)
 help(flights)
@@ -31,10 +31,57 @@ flights_U_A_D_May_5 <- flight %>% filter(carrier %in% c("UA", "AA", "DL") &
 flights %>% mutate(speed = distance / (air_time / 60)) %>% 
                     select(carrier, flight, speed) %>% arrange(desc(speed))
                     top_n(speed, 10)
-                    
-# (g) Find a list of carriers with a column ratio which denotes the number of flights
+     
+# (g) Exchange the values of departure time and arrvial time in minute
+# after midnight.
+# Example: departure time 722 given by HMinutes is in minutes
+# after midnight 442!
+flights <- flight %>% mutate(
+  # %/% gets the hours 722 %/% 100 = 7
+  # %% gets the minutes 722 %% 100 = 22
+  dep_minutes = (dep_time %/% 100) * 60 + (dep_time %% 100), 
+  arr_minutes = (arr_time %/% 100) * 60 + (arr_time %% 100)) %>% 
+  select(dep_minutes, arr_minutes
+)
+
+# (h) Add a column speed which denotes the average speed of the flight
+# and determine the carrier, flight of the top 10 values of speed.
+flights <- flight %>% mutate(
+  speed = (distance / air_time * 60)) %>% select(carrier, flight, speed) %>%
+  arrange(desc(speed)) %>% top_n(10, speed)
+
+# (i) Find a list of carriers with a column ratio which denotes the number of flights
 # with arr_delay less than 10 minutes to the total number of flights. The list 
 # should be sorted by ratio
-flights %>% filter(!is.na(arr_delay)) %>% count(arr_delay < 10, carrier) %>%
-  group_by(carrier) %>% mutate(nof = sum(n), del_ratio = n / sum(n)) %>% 
-  filter("arr_delay < 10" == TRUE) %>% arrange(desc(del_ratio))
+flights %>% filter(!is.na(arr_delay)) %>% # Ignore flights that are unknown
+  mutate(bool_del = if_else(arr_delay < 10, 1, 0)) %>% # New Col flights on time
+  group_by(carrier) %>% # group data by carrier to do the calculations
+  mutate(
+    nof = n(), # count total number of flights
+    ndel = sum(bool_del), # count the number of "on time" flights carrier
+    del_ratio = ndel / nof # Get the ratio from total and number of on time flights
+    ) %>%
+  select(carrier, nof, ndel, del_ratio) %>% # select relevant cols
+  unique() %>% # remove duplicates
+  arrange(desc(del_ratio) # sort carriers on time desc
+)
+
+# (j) Find a list which denotes for every month the carrier with highest
+# ratio which denotes the number of flights with arr delay less than
+# 10 minutes to the total number of flights. The list should have
+# the columns month, carrier, number of flights of the carrier in that
+# month and ratio.
+
+
+
+# (l)  Determine a table that shows, for each airline (carrier), the flight
+# connection given by the airports of dest und origin that occurred
+# most frequently in 2013. The table should contain only the columns
+# names of airline, destination, origin and frequency and be sorted
+# by frequency in descending order. You can find the names of the
+# carrier from the dataset airlines and the names of the airports
+# from the dataset airports.
+flights %>% count(carrier, origin, dest) %>% group_by(carrier)
+
+
+rm(list = ls())
